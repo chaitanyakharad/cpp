f922a9999d71c71d13a721a46d1dc0668bdac5f3
Chaitanya Adinath Kharad <chaitanyakharad5586@gmail.com>
2025-11-19 17:41:48 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    char op;
+    int n1,n2;
+    cout<<"Enter the operator : + , - , * , / :";
+    cin>>op;
+
+    cout<<"Enter two operator : ";
+    cin>>n1>>n2;
+
+    switch (op)
+    {
+    case '+' : 
+        cout<<n1<<"+"<<n2<<"="<<n1+n2;
+        break;
+
+    case '-' :
+        cout<<n1<<"-"<<n2<<"="<<n1-n2;
+        break;
+
+    case '*' :
+        cout<<n1<<"*"<<n2<<"="<<n1*n2;
+        break;
+
+    case '/' :
+        cout<<n1<<"/"<<n2<<"="<<n1/n2;
+        break;
+    
+    default:
+    cout<<"error";
+        break;
+    }
+}
