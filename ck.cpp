dcf7d388baaa17b471f279b059cea0551e7e0dc8
Chaitanya Adinath Kharad <chaitanyakharad5586@gmail.com>
2025-11-21 10:08:14 +0530

    Add files via upload

---
+++ ck.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5; // You can change this value to print more or fewer rows
+
+    for(int i = n; i >= 1; i--)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+// defult constructor
+// default contructor la call dyaychi garaj nahi fct obj create kela tri pn chalt 
+#include<iostream>
+using namespace std;
+class student {
+public: 
+    student()
+    {
+        cout<<"this is my constructor"<<endl;
+    }
+};
+int main()
+{
+    student s1;
+    student s2;
+}
